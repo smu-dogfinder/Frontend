f4e74e880d77d5159e248a6deab46155a6fef71e
yourim01 <162237840+yourim01@users.noreply.github.com>
2025-10-29 22:16:26 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 const BACKEND_URL = process.env.VITE_BACKEND_URL || 'http://localhost:8000';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+           nodePolyfills({
+             include: ['crypto'],
+           }),
+  ],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
